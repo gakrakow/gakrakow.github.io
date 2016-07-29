ff6c527103fba2b4cfff507794a53c03ec59b2f5
gakrakow <greg.krakow@dnr.ga.gov>
2016-07-29 08:59:08 -0400

    Fixed several bugs with csv output. Other minor fixes and updates.

--- rare_elements.js
+++ rare_elements.js
@@ -3,17 +3,20 @@ function filenameDate(dateval) {
     var year = dateval.getFullYear();
     var month = ("0" + (dateval.getMonth() + 1)).slice(-2);
     var date = ("0" + dateval.getDate()).slice(-2);
-    var filenameDate = year + month + date;
+    var filenameDate = '-' + year + '-' + month + '-' + date;
     return filenameDate;
     }
 
 function makeCsvFilename(header_vals) {
-    // This cleans up and formats CSV file names.
-    // Some day you are gonna have to figure out how to do this with much fewer regular expressions.
+    // This cleans up and formats CSV file names. 
+    // The regex cleanup part of this should probably be put into a different function. Similar code is used in another place in the element_lists.html page.
     var csv_filename = header_vals.rank + ' ' + header_vals.group + ' ' + header_vals.status;
-    csv_filename = csv_filename.replace(/,/g,"").replace(/\(/g,"").replace(/\)/g,"");
-    csv_filename = csv_filename.replace(/ /g,"-").replace(/\&/,"-").replace('<br>',"-");
-    csv_filename = csv_filename.replace(/--/g,"-").replace(/--/g,"-").replace(/--/g,"-").toLowerCase();
-    return csv_filename
+    csv_filename = csv_filename.replace(/\s|\.|,|:|\(|\)|&|(<br>)/g,"-");
+    csv_filename = csv_filename.replace(/-+/g,"-");
+    csv_filename = csv_filename.replace(/^-|-$/,"");
+    return csv_filename;
     }
+
+
+
 
