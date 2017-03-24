fe820c6dc574749c6f5954b8fa1480a020067446
gakrakow <greg.krakow@dnr.ga.gov>
2017-03-24 14:25:23 -0400

    Several small updates to about-this-data, element_lists, and added a draft range_map3.html page.

--- rare_elements.js
+++ rare_elements.js
@@ -1,3 +1,7 @@
+function getURLParameter(name) {
+    return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [, ""])[1].replace(/\+/g, '%20')) || null
+    }
+
 function filenameDate(dateval) {
     var dateval = new Date(dateval);
     var year = dateval.getFullYear();
@@ -11,9 +15,19 @@ function makeCsvFilename(header_vals) {
     // This cleans up and formats CSV file names. 
     // The regex cleanup part of this should probably be put into a different function. Similar code is used in another place in the element_lists.html page.
     var csv_filename = header_vals.rank + ' ' + header_vals.group + ' ' + header_vals.status;
+    
+    // Replace whitespace characters, period, comma, colon, right & left parenthesis, and <br>, with dash.
     csv_filename = csv_filename.replace(/\s|\.|,|:|\(|\)|&|(<br>)/g,"-");
+    
+    // Replace forward slash and back slash with dash * This should only be done before joining with rest of file name.
+    //csv_filename = csv_filename.replace(/\//g, '-');
+    
+    // Replace multiple dashes in a row
     csv_filename = csv_filename.replace(/-+/g,"-");
+    
+    // Get rid of dashes at beginning or end of line
     csv_filename = csv_filename.replace(/^-|-$/,"");
+    
     return csv_filename;
     }
 
